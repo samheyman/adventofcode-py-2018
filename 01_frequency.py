eb429e499670300a03cbca57f91b51047dec5edb
Sam Heyman <samheyman@Sams-MacBook-Pro.local>
2018-12-02 17:51:58 +0100

    Day 1

---
+++ 01_frequency.py
@@ -0,0 +1,18 @@
+import urllib.request
+import os
+
+frequency_shift = 0
+list_of_frequencies = []
+i=0
+
+for loop in range (200):
+    with open('data_01.txt', 'r') as data:
+        for shift in data:
+            frequency_shift+=int(shift)
+            if (frequency_shift in list_of_frequencies) and (frequency_shift > 587):
+                print("Frequency {} already detected!".format(frequency_shift))
+                break
+            else:
+                list_of_frequencies.append(frequency_shift)
+            #print("Adding {}. Total is now {}".format(shift, frequency_shift))
+    print("Loop #{}".format(loop+2))
